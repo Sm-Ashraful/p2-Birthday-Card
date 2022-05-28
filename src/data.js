934ddf64847bdb625b927b03ec18b59008e75f2a
Ash <smashraful1995@gmail.com>
2022-05-28 16:34:02 +0600

    initial commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: "Mehedi Hasan Rabby",
+    age: 25,
+    image:
+      "https://scontent.fjsr3-1.fna.fbcdn.net/v/t1.6435-9/57066152_1253223154827530_2998553072927506432_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=174925&_nc_eui2=AeGm2jD1dLcDRZRDPpjiFNfuVHcyC9BCjSlUdzIL0EKNKcAwUbBJ9t7SC179SQJw-N0_-ofgWHDrOdWJxshl5y6O&_nc_ohc=LyXYQ3moh4oAX9_E9_Y&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT8B-a7PeqVvoHh0neyoRslrmDdrIqFgEcs7lPytudoroQ&oe=62701BC1",
+  },
+  {
+    id: 2,
+    name: "SI Jewel",
+    age: 20,
+    image:
+      "https://scontent.fjsr3-1.fna.fbcdn.net/v/t1.6435-9/89750905_824043241429322_8560008428112576512_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_eui2=AeE9wNGdM54frLaI_JwcbPaz3eWJaJPZD1rd5Ylok9kPWmY9StYq1Esi9H_tTq1wp1YtcF0NAtnwIhAIPqq-Igh8&_nc_ohc=CtJSFrBjNSgAX8LpyiF&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT-5ItNLot0LfPdvMhLfU67KHFFnl5m7-dum6LJkzcgcYg&oe=626F501D",
+  },
+  {
+    id: 3,
+    name: "Sm Ashraful Islam",
+    age: 25,
+    image:
+      "https://scontent.fjsr3-1.fna.fbcdn.net/v/t1.6435-9/51499945_997217583795570_3905366023691304960_n.jpg?stp=dst-jpg_s552x414&_nc_cat=104&ccb=1-5&_nc_sid=174925&_nc_eui2=AeEFYWi8_P8XZw7OPyV5VAYHy4tEnO0pwTjLi0Sc7SnBOFr1PWAAKRo84w9Lsgf6xFjQxwjf39WMrPRcQM5KNt_B&_nc_ohc=6phJ9bst0rIAX9VZkti&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT_oOOEyIm1h-mIvGqk_hqjmeUpmpyEviWmdakmhQ9UnUw&oe=626CFDA1",
+  },
+];
